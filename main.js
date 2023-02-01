b47af9eb42676e75f1e5d9681f6d6b434c59061b
ANDREY <1yamzin98@mail.ru>
2023-02-01 22:41:03 +0700

    зафиксировал размер текста

--- main.js
+++ main.js
@@ -3,24 +3,26 @@ let buttons = document.querySelectorAll('.button');
 let inputNumbers = document.querySelector('.input');
 let inputSaveNumbers = document.querySelector('.save-input');
 let numders = [];
+let numbersTwo = [];
 
+// function solutionMethod(meth){
 
-function setNumbers(num){
-// console.log( typeof +num)
-}
-
-function solutionMethod(meth){
-
-}
+// }
 parentButton.addEventListener('click', function(e){
     if(e.target.innerHTML > -1 && e.target.innerHTML <= 9){
-        numders.push(e.target.innerHTML);
-        // console.log(e.target.innerHTML)
-        let setText = document.createElement('span')
-        setText.innerHTML = e.target.innerHTML;
-        inputNumbers.append(setText)
-        // inputNumbers.textContent = e.target.innerHTML;
-    }else{
-        console.log('no')
+        setNumbers(e.target.innerHTML);
+    }else if(e.target.innerHTML == '='){
+        console.log(numders)
     }
+
 });
+
+function setNumbers(num){
+    if(numders.length > 16){
+    }else{
+        numders.push(num);
+        let setText = document.createElement('span')
+        setText.innerHTML = num;
+        inputNumbers.prepend(setText)
+    }   
+}
