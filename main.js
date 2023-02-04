855e1630ad0312c7630392d0001cb0a97525ef6e
ANDREY <1yamzin98@mail.ru>
2023-02-04 19:04:42 +0700

    сделал удаление

--- main.js
+++ main.js
@@ -5,20 +5,52 @@ let inputSaveNumbers = document.querySelector('.save-input');
 let numders = [];
 let numbersTwo = [];
 
-// function solutionMethod(meth){
-
-// }
-parentButton.addEventListener('click', function(e){
-    if(e.target.innerHTML > -1 && e.target.innerHTML <= 9){
-        setNumbers(e.target.innerHTML);
-    }else if(e.target.innerHTML == '='){
-        console.log(numders)
+function removeLastSpan(){
+    inputNumbers.removeChild(inputNumbers.firstChild);
+    numders.pop();
+//проработать ошибку постого удаления
+}
+//функция удаляет поле ввода основное
+function removeInputLine(){
+    while(inputNumbers.firstChild){
+        inputNumbers.removeChild(inputNumbers.firstChild);
     }
+    return numders = [];
+}
+//функция удаляет все поля ввода
+function removeAllFields(){
+    while(inputNumbers.firstChild){
+        inputNumbers.removeChild(inputNumbers.firstChild)
+  
+    }
+    while(inputSaveNumbers.firstChild){
+        inputSaveNumbers.removeChild(inputSaveNumbers.firstChild)
+    }
+    return numders = [];
+}
 
+
+
+
+parentButton.addEventListener('click', function(e){
+    if(e.target.innerHTML > -1 && e.target.innerHTML <= 9 || e.target.innerHTML == ','){
+        setNumbers(e.target.innerHTML);
+    }else if(e.target.innerHTML == 'CE'){
+        removeInputLine()
+    }else if(e.target.innerHTML == 'C'){
+        removeAllFields()
+    }else if(e.target.innerHTML == '←'){
+        removeLastSpan()
+    }else if (e.target.innerHTML == '+'){
+        numbersTwo = numders.slice();
+        //вот тут доделать нужно, когда плюс нажимаем
+    }
+console.log(e.target.innerHTML)
 });
 
 function setNumbers(num){
     if(numders.length > 16){
+        
     }else{
         numders.push(num);
         let setText = document.createElement('span')
@@ -26,3 +58,13 @@ function setNumbers(num){
         inputNumbers.prepend(setText)
     }   
 }
+
+let arr = [1, 2, 3]
+let arr2 = []
+
+arr2 = arr.slice();
+
+arr2[3] = 4;
+
+console.log(arr)
+console.log(arr2)
